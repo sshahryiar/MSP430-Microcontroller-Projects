8955edcb8ba400f1c2b82256b168f7d9d44cfb96
MicroArena <sshahryiar@users.noreply.github.com>
2023-04-15 21:31:58 +0600

    Add files via upload

---
+++ MODBUS_Hygrometer/modbus_hygrometer/Debug/Libraries/lcd.d
@@ -0,0 +1,19 @@
+# FIXED
+
+Libraries/lcd.obj: ../Libraries/lcd.c
+Libraries/lcd.obj: ../Libraries/lcd.h
+Libraries/lcd.obj: C:/ti/ccs930/ccs/ccs_base/msp430/include/msp430.h
+Libraries/lcd.obj: C:/ti/ccs930/ccs/ccs_base/msp430/include/msp430f149.h
+Libraries/lcd.obj: C:/ti/ccs930/ccs/ccs_base/msp430/include/in430.h
+Libraries/lcd.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_20.2.0.LTS/include/intrinsics.h
+Libraries/lcd.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_20.2.0.LTS/include/intrinsics_legacy_undefs.h
+Libraries/lcd.obj: D:/Current\ Works/MODBUS_Hygrometer/modbus_hygrometer/Libraries/delay.h
+
+../Libraries/lcd.c: 
+../Libraries/lcd.h: 
+C:/ti/ccs930/ccs/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccs930/ccs/ccs_base/msp430/include/msp430f149.h: 
+C:/ti/ccs930/ccs/ccs_base/msp430/include/in430.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_20.2.0.LTS/include/intrinsics.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_20.2.0.LTS/include/intrinsics_legacy_undefs.h: 
+D:/Current\ Works/MODBUS_Hygrometer/modbus_hygrometer/Libraries/delay.h: 
---
+++ MODBUS_Hygrometer/modbus_hygrometer/Debug/main.d
@@ -0,0 +1,27 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccs930/ccs/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccs930/ccs/ccs_base/msp430/include/msp430f149.h
+main.obj: C:/ti/ccs930/ccs/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_20.2.0.LTS/include/intrinsics.h
+main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_20.2.0.LTS/include/intrinsics_legacy_undefs.h
+main.obj: D:/Current\ Works/MODBUS_Hygrometer/modbus_hygrometer/Libraries/delay.h
+main.obj: D:/Current\ Works/MODBUS_Hygrometer/modbus_hygrometer/Libraries/lcd.h
+main.obj: D:/Current\ Works/MODBUS_Hygrometer/modbus_hygrometer/Libraries/delay.h
+main.obj: D:/Current\ Works/MODBUS_Hygrometer/modbus_hygrometer/Libraries/lcd_print.h
+main.obj: D:/Current\ Works/MODBUS_Hygrometer/modbus_hygrometer/Libraries/lcd.h
+main.obj: D:/Current\ Works/MODBUS_Hygrometer/modbus_hygrometer/Libraries/delay.h
+
+../main.c: 
+C:/ti/ccs930/ccs/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccs930/ccs/ccs_base/msp430/include/msp430f149.h: 
+C:/ti/ccs930/ccs/ccs_base/msp430/include/in430.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_20.2.0.LTS/include/intrinsics.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_20.2.0.LTS/include/intrinsics_legacy_undefs.h: 
+D:/Current\ Works/MODBUS_Hygrometer/modbus_hygrometer/Libraries/delay.h: 
+D:/Current\ Works/MODBUS_Hygrometer/modbus_hygrometer/Libraries/lcd.h: 
+D:/Current\ Works/MODBUS_Hygrometer/modbus_hygrometer/Libraries/delay.h: 
+D:/Current\ Works/MODBUS_Hygrometer/modbus_hygrometer/Libraries/lcd_print.h: 
+D:/Current\ Works/MODBUS_Hygrometer/modbus_hygrometer/Libraries/lcd.h: 
+D:/Current\ Works/MODBUS_Hygrometer/modbus_hygrometer/Libraries/delay.h: 
