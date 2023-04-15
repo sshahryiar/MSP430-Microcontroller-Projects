75ea8a8c05f1c14c4d49df147b208aec452759f6
MicroArena <sshahryiar@users.noreply.github.com>
2023-04-15 21:43:52 +0600

    Add files via upload

---
+++ PT100 Transmitter/PT100_to_Voltage_Transmitter/Debug/My_Libraries/subdir_vars.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../My_Libraries/ST7735.c 
+
+C_DEPS += \
+./My_Libraries/ST7735.d 
+
+OBJS += \
+./My_Libraries/ST7735.obj 
+
+OBJS__QUOTED += \
+"My_Libraries\ST7735.obj" 
+
+C_DEPS__QUOTED += \
+"My_Libraries\ST7735.d" 
+
+C_SRCS__QUOTED += \
+"../My_Libraries/ST7735.c" 
+
+
